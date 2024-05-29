730514f95efd09240026aabd492e99f94868e9dd
Tiagosvi <102734829+Tiagosvi@users.noreply.github.com>
2024-05-29 11:19:52 -0300

    Add files via upload

---
+++ Guia8.py
@@ -0,0 +1,54 @@
+#Ejercicio 1:
+def contar_lineas (nombrearchivo : str) -> int:
+        f=open(nombrearchivo)
+        lineas:list [str] = f.readlines()
+        cantidad:int = len(lineas)
+        return cantidad
+
+def existe_palabra(palabra : str, nombrearchivo : str) -> bool:
+    f=open(nombrearchivo)
+    for i in f.readlines():
+        if palabra in i:
+            return True
+    return False
+
+def cantidad_de_apariciones (palabra: str, nombrearchivo: str) -> int:
+    f=open(nombrearchivo)
+    cantidad:int = 0
+    for i in f.readlines():
+        if palabra in i:
+            cantidad += 1
+
+    return cantidad
+
+#Ejercicio 2:
+
+def clonar_sin_comentarios(nombrearchivo: str):
+    f=open(nombrearchivo)
+    sincomentarios:list[str] = []
+    for linea in f.readlines():
+       if not linea[0] ==  "#":
+            sincomentarios.append(linea)
+    res = open ("sincomentarios"+nombrearchivo, "w")
+    res.writelines(sincomentarios)
+    res.close()
+    f.close()
+
+#hola
+
+#Ejercicio 3:
+
+def reverso(nombrearchivo: str):
+    f=open(nombrearchivo)
+    lineas = f.readlines ()
+    i = len(lineas)-1
+    reverso:list[str] = []
+    while i > 0:
+        reverso.append (lineas [i])
+        i-=1
+    res = open ("reverso.txt","w")
+    res.writelines(reverso)
+    res.close()
+    f.close()
+
+
