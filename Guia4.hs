d049925e9bc871eb6317b1d5608c587356424fa1
Tiagosvi <tiago.svigui@gmail.com>
2024-06-16 20:56:02 -0300

    aaa

--- Guia4.hs
+++ Guia4.hs
@@ -10,6 +10,26 @@ parteEntera x | x<1 && x>=0 =0
               | otherwise =(-1)+parteEntera (x+1)
 
 
+sumaImpares :: Int -> Int 
+sumaImpares 0 = 0
+sumaImpares a = sumaImpares (a-1) + (a*2 - 1) 
+
+
+sumaDigitos :: Int -> Int
+sumaDigitos x | x == 0 = 0
+              | otherwise = (rem x 10) + sumaDigitos (div x 10)
+
+
+
+todosDigitosIguales :: Int -> Bool
+todosDigitosIguales x |  x < 10 = True
+                      | x < 100 = rem x 10 == div x 10 
+                      | otherwise = todosDigitosIguales (rem x 100) 
+
+
+
+
+
 
 
 
